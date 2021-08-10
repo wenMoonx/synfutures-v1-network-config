aad6a94351d51a6c14a73f70bab100b678fab007
jxiaox <jxiaox@hotmail.com>
2021-08-10 16:54:47 +0800

    fix json config export bug

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import mainnetConfig from './config/mainnet.json';
-import kovanConfig from './config/kovan.json';
-import polygonConfig from './config/polygon.json';
+import * as mainnetConfig from './config/mainnet.json';
+import * as kovanConfig from './config/kovan.json';
+import * as polygonConfig from './config/polygon.json';
 
 export { mainnetConfig, kovanConfig, polygonConfig };
