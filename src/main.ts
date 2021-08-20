9dfe7457134b460a8e1c2d2a9b165df8caf8f564
jxiaox <jxiaox@hotmail.com>
2021-08-20 10:52:49 +0800

    add bsc config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as mainnetConfig from './config/mainnet.json';
 import * as kovanConfig from './config/kovan.json';
 import * as polygonConfig from './config/polygon.json';
+import * as bscConfig from './config/bsc.json';
 
-export { mainnetConfig, kovanConfig, polygonConfig };
+export { mainnetConfig, kovanConfig, polygonConfig, bscConfig };
