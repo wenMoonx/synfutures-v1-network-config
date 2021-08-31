21d38fd84c2d52f17b1d3e770a0b9a59097b2adb
jxiaox <jxiaox@hotmail.com>
2021-08-31 14:43:48 +0800

    export arbitrumConfig

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import * as ethereumConfig from './config/ethereum.json';
 import * as kovanConfig from './config/kovan.json';
 import * as polygonConfig from './config/polygon.json';
 import * as bscConfig from './config/bsc.json';
+import * as arbitrumConfig from './config/arbitrum.json';
 
-export { ethereumConfig, kovanConfig, polygonConfig, bscConfig };
+export {
+  ethereumConfig,
+  kovanConfig,
+  polygonConfig,
+  bscConfig,
+  arbitrumConfig,
+};
